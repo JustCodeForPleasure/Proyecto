73e42c69a39c629009b9223c931d0e1311d3f674
Joan García <just.code.for.pleasure@gmail.com>
2017-09-12 17:48:59 -0500

    -joan- Se diseño la primera interfaz de la aplicación

---
+++ index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Proyecto Arduino</title>
+	<!--Links-->
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<!--FONTS (Anton | Hind)-->
+	<link href="https://fonts.googleapis.com/css?family=Anton|Hind" rel="stylesheet">
+
+</head>
+<body>
+	<header>
+		<h1>Sistema de Control y Monitoréo de Arduino</h1>
+		<h2>Grupo CCRED</h2>
+	</header>
+
+	<section class="contenedor">
+		<h2 id="encabezado">Lista de Arduinos conectados a la Aplicación</h2>
+
+		<article>
+			<table>
+			  <tr>
+			    <th>ID Arduino</th>
+			    <th>MAC Arduino</th>
+			    <th>Temperatura</th>
+			    <th>Humedad</th>
+			    <th>Alarma 1</th>
+			    <th>Alarma 2</th>
+			    <th>Alarma 3</th>
+			    <th>Alarma 4</th>
+
+			  </tr>
+			  <tr>
+			    <td>2</td>
+			    <td>1111:2222:3333:4444</td>
+			    <td>10.99</td>
+			    <td>25.66</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			  </tr>
+			  <tr>
+			    <td>3</td>
+			    <td>1111:2222:3333:4444</td>
+			    <td>10.99</td>
+			    <td>25.66</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			  </tr>
+			  <tr>
+			    <td>4</td>
+			    <td>1111:2222:3333:4444</td>
+			    <td>10.99</td>
+			    <td>25.66</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			  </tr>
+			  <tr>
+			    <td>5</td>
+			    <td>1111:2222:3333:4444</td>
+			    <td>10.99</td>
+			    <td>25.66</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			  </tr>
+			  <tr>
+			    <td>6</td>
+			    <td>1111:2222:3333:4444</td>
+			    <td>10.99</td>
+			    <td>25.66</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			  </tr>
+			  <tr>
+			    <td>7</td>
+			    <td>1111:2222:3333:4444</td>
+			    <td>10.99</td>
+			    <td>25.66</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			  </tr>
+			  <tr>
+			    <td>2</td>
+			    <td>1111:2222:3333:4444</td>
+			    <td>10.99</td>
+			    <td>25.66</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			  </tr>
+			  <tr>
+			    <td>3</td>
+			    <td>1111:2222:3333:4444</td>
+			    <td>10.99</td>
+			    <td>25.66</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			  </tr>
+			  <tr>
+			    <td>4</td>
+			    <td>1111:2222:3333:4444</td>
+			    <td>10.99</td>
+			    <td>25.66</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			  </tr>
+			  <tr>
+			    <td>5</td>
+			    <td>1111:2222:3333:4444</td>
+			    <td>10.99</td>
+			    <td>25.66</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			  </tr>
+			  <tr>
+			    <td>6</td>
+			    <td>1111:2222:3333:4444</td>
+			    <td>10.99</td>
+			    <td>25.66</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			  </tr>
+			  <tr>
+			    <td>7</td>
+			    <td>1111:2222:3333:4444</td>
+			    <td>10.99</td>
+			    <td>25.66</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			    <td>Apagada</td>
+			  </tr>
+			</table>
+		</article>
+	</section>
+
+	<footer>
+		<div class="nosotros">
+			<p>Grupo CCRED - Todos los derechos reservados</p>	
+		</div>
+		
+	</footer>
+</body>
+</html>
